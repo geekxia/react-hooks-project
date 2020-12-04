72cd496ec6a1237fb26f4e6301923bd398f0e113
wcs <84517550@qq.com>
2020-12-04 22:04:42 +0800

    基础表单

--- src/views/index.js
+++ src/views/index.js
@@ -4,6 +4,7 @@ import {
 } from '@ant-design/icons'
 
 const ReduxStudy = loadable(()=>import('./home/ReduxStudy'))
+const jichuBiaoDan = loadable(()=>import('./pages/jichuBiaoDan'))
 
 
 export default [ // eslint-disable-line
@@ -17,6 +18,19 @@ export default [ // eslint-disable-line
         text: '学习Redux',
         path: '/',
         component: ReduxStudy
+      }
+    ]
+  },
+  {
+    id: 12,
+    text: '表单页',
+    icon: <DotChartOutlined />,
+    children: [
+      {
+        id: 1111,
+        text: '基础表单',
+        path: '/jichubiaodan',
+        component: jichuBiaoDan
       }
     ]
   }
---
+++ src/views/pages/jichuBiaoDan.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default props=>{
+    return (
+        <div>
+            <h1>基础表单</h1>
+        </div>
+    )
+}
