bb726836a7df30b1d5fc19a8cd44ab506d6cffbd
kzk <596508302@qq.com>
2020-12-04 17:25:09 +0800

    kzk的init

---
+++ src/store/actionTypes.js
@@ -0,0 +1,14 @@
+// 这个action.type常量，相当于字典索引，避免在团队协作时产生命名冲突
+// action 是 视图和Store之间的纽带，actions和reducer是间接关系
+// action 是怎么从视图中抵达Store？是使用 dispatch(action)
+
+const CHANGE_MSG = 'CHANGE_MSG'
+const ADD_FOO_COUNT = 'ADD_FOO_COUNT'
+
+const AJAX_MUSIC_LIST = 'AJAX_MUSIC_LIST'
+
+export default {
+  CHANGE_MSG,
+  ADD_FOO_COUNT,
+  AJAX_MUSIC_LIST
+}
---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+import axios from './axios'
+
+export function fetchQqMusic(params) {
+  return axios({
+    url: '/soso/fcgi-bin/client_search_cp',
+    method: 'GET',
+    params
+  })
+}
+
+export default {
+  fetchQqMusic
+}
