865a56e36af884bdd2365bc1fcff2b6d05662d62
shawn <1104566127@qq.com>
2020-12-05 17:09:39 +0800

    shawn

---
+++ src/views/home/ShawnLearn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => {
+  return (
+    <div>
+      <h1>shawn</h1>
+    </div>
+  )
+}
--- src/views/index.js
+++ src/views/index.js
@@ -4,6 +4,8 @@ import {
 } from '@ant-design/icons'
 
 const ReduxStudy = loadable(()=>import('./home/ReduxStudy'))
+const ShawnLearn = loadable(()=>import('./home/ShawnLearn'))
+
 
 export default [
   {
@@ -16,6 +18,19 @@ export default [
         text: '学习Redux哟',
         path: '/',
         component: ReduxStudy
+      }
+    ]
+  },
+  {
+    id: 12,
+    text: 'Shawn',
+    icon: <DotChartOutlined />,
+    children: [
+      {
+        id: 1211,
+        text: 'learn more',
+        path: '/shawnlearn',
+        component: ShawnLearn
       }
     ]
   }
