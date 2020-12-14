1f6e6ad23827e6dfaa0f18998fff752e203b2b1b
WCS <johndoe@example.com>
2020-12-14 12:54:47 +0800

    。
    。

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,9 +1,96 @@
+import { useEffect } from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
+import './style.scss'
+import api from '@/utils/api'
+import { useHistory } from 'react-router-dom'
 
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 19 }
+}
+
+const tailLayout = {
+  wrapperCol: { offset: 4, span: 19 }
+}
 
 export default props => {
-    return(
-        <div>
-            <h1>登录页面</h1>
-        </div>
-    )
+
+  const history = useHistory()
+
+  // 登录提交
+  const onFinish = (values) => {
+    console.log('Success:', values)
+    api.fetchLogin(values).then(res=>{
+      console.log('登录成功', res)
+      if(res && res.token) {
+        // 把登录状态（鉴权）存储起来
+        localStorage.setItem('token', res.token)
+        // 跳转到首页
+        // location.href='/#/'
+        history.replace('/')
+        // 调用App组件传递过来的onLogin方法，刷新 isLogin
+        props.onLogin()
+      }
+    })
+  }
+
+  useEffect(()=>{
+    location.href = '/#/login'
+    return undefined
+  }, [])
+
+  return (
+    <div className='qf-login'>
+      <div>
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+              {
+                pattern: /^[a-zA-Z][a-zA-Z]{1,19}$/,
+                message: '用户名格式有误'
+              }
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="密 码"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+            <Checkbox>记住密码</Checkbox>
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
